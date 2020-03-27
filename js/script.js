b9527edc1e299c6de49f946afd35900889529f45
Juan Angel Ruiz <juanangel.p@gmail.com>
2020-03-27 16:39:45 -0300

    arreglo de responsive

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ $('#toogle-menu').click(function(){
 $(window).resize(function(){
     if($(document).width() > 767){
         $('.menu').css({'display' : 'flex'});
+        $('section').css({'min-height': $(document).width()- 88 });
     }
     if($(document).width() < 768){
         $('.menu').css({'display' : 'none'});
+        $('section').css({'min-height': $(document).width()- 96 });
     }
 })
