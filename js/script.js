69906d30dc2b04e06f15f629a333f4b2f6f48709
Juan Angel Ruiz <juanangel.p@gmail.com>
2020-03-27 02:33:32 -0300

    repair bug menu responsive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $('#toogle-menu').click(function(){
     $(this).next().slideToggle();
 });
+$(window).resize(function(){
+    if($(document).width() > 767){
+        $('.menu').css({'display' : 'flex'});
+    }
+    if($(document).width() < 768){
+        $('.menu').css({'display' : 'none'});
+    }
+})
